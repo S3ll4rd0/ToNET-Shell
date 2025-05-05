5dede6af1d6e7cc0c3aac8433b45d99b4066ba03
antonio.salmeron <antonio-nicolas.salmeron@aquatec.es>
2025-05-05 08:38:02 +0200

    Monitor funcional y lógica en Controller.

--- Shell._WinForms/MainForm.cs
+++ Shell._WinForms/MainForm.cs
@@ -1,44 +1,13 @@
-using Microsoft.VisualBasic.ApplicationServices;
-using Microsoft.VisualBasic.Devices;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Diagnostics.Metrics;
-using System.Diagnostics.Tracing;
-using System.Drawing;
-using System.IO;
-using System.Management;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Timers;
-using System.Windows.Forms;
-
 namespace Shell._WinForms
 {
     public partial class MainForm : Form
     {
-        private readonly PerformanceCounter _receiveCounter;
-        private readonly PerformanceCounter _sendCounter;
-        private readonly PerformanceCounter _cpuCounter;
-        private readonly PerformanceCounter _ramCounter;
-        private readonly DriveInfo[] _drives;
-        private static System.Timers.Timer? timer;
-
-        private const string DOWNARROW = "\u2191";
-        private const string UPARROW = "\u2193";
-        private string _monitorText;
-        private string _cpumonitor;
-        private string _rammonitor;
-        private string _upmonitor;
-        private string _downmonitor;
-        private int _monitorBlocks;
-        private int _cpu;
-        private int _ram;
-        private int _up;
-        private int _down;
+        private MonitorController _monitorController;
 
         private string[] _cachedCmds;
+        private string _userLine;
         private string _cachedSt;
+        private string _input;
         private string _user;
         private string _domn;
         private string _path;
@@ -46,17 +15,12 @@ namespace Shell._WinForms
         private int _domnln;
         private int _pathln;
 
+        private string[] args;
+
         public MainForm()
         {
-            _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            _cpuCounter.NextValue();
-            _ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-            _ramCounter.NextValue();
-            _sendCounter = new PerformanceCounter("Network Interface", "Bytes Sent/sec", "Realtek USB GbE Family Controller");
-            _sendCounter.NextValue();
-            _receiveCounter = new PerformanceCounter("Network Interface", "Bytes Received/sec", "Realtek USB GbE Family Controller");
-            _receiveCounter.NextValue();
-            _drives = DriveInfo.GetDrives();
+            InitializeComponent();
+            _monitorController = new(this.Monitor);
 
             _cachedCmds = new string[20];
             _user = $"{Environment.UserName}";
@@ -66,211 +30,16 @@ namespace Shell._WinForms
             _domnln = _domn.Length + 1;
             _pathln = _path.Length + 1;
 
-            InitializeComponent();
             //ShowMonitor();
-            //ShowUserLine();
+            ShowUserLine();
             //ShowShellHeader();
             // 43 tamaño mitad - 86 total
             //Shell.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-ABCDEFGHIJKLMNOPQRSTUVWXYZ-ABCDEFGHIJKLMNOPQRSTUVWXYZ-ABCDE";
             //MessageBox.Show(Shell.Text.Length + "");
-
-            Thread.Sleep(1000);
-            timer = new System.Timers.Timer(2000);
-            timer.Elapsed += UpdateMonitorValues;
-            timer.AutoReset = true;
-            timer.Start();
-        }
-
-        private void UpdateMonitorValues(object sender, ElapsedEventArgs e)
-        {
-            _monitorText = string.Empty;
-            _monitorBlocks = 0;
-
-            ReadComputerValues();
-            ReadDiskValues();
-
-            if (Monitor.InvokeRequired)
-                Monitor.Invoke(new Action(() => UpdateMonitorText()));
-            else
-                UpdateMonitorText();
-        }
-
-        private void ReadComputerValues()
-        {
-            _cpu = Convert.ToInt32(_cpuCounter.NextValue());
-            _ram = Convert.ToInt32(_ramCounter.NextValue());
-            _up = Convert.ToInt32(_sendCounter.NextValue()) / 1024;
-            _down = Convert.ToInt32(_receiveCounter.NextValue()) / 1024;
-
-            _cpumonitor = $"  CPU     {GetUsageBar(_cpu)} {_cpu:00}% ";
-            _rammonitor = $"  RAM     {GetUsageBar(_ram)} {_ram:00}% ";
-            _upmonitor = $"  NET {UPARROW}   {GetUsageBar(_up)} {_up:00}% ";
-            _downmonitor = $"  NET {DOWNARROW}   {GetUsageBar(_down)} {_down:00}% ";
-            
-            _monitorText = $"{_cpumonitor}{_rammonitor}\n{_upmonitor}{_downmonitor}\n";
-            _monitorBlocks = 4;
-        }
-
-        private void ReadDiskValues()
-        {
-            for (int i = 0; i < _drives.Length; i++)
-            {
-                try
-                {
-                    DriveInfo drive = _drives[i];
-                    string dName = drive.Name[..1];
-                    float totalDisk = Convert.ToSingle(drive.TotalSize / 1024 / 1024 / 1024);
-                    float freeDisk = Convert.ToSingle(drive.AvailableFreeSpace / 1024 / 1024 / 1024);
-                    int usedDisk = Convert.ToInt32((totalDisk - freeDisk) * 100 / totalDisk);
-
-                    if ((i % 2) == 0)
-                        _monitorText += $"  UNIT {dName}  {GetUsageBar(usedDisk)} {usedDisk:00}% ";
-                    else
-                        _monitorText += $"  UNIT {dName}  {GetUsageBar(usedDisk)} {usedDisk:00}% \n";
-                    _monitorBlocks++;
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-            }
-        }
-
-        private static string GetUsageBar(int usage)
-        {
-            string bar = string.Empty;
-            int bars = usage / 4 < 1 ? 1 : usage / 4;
-            for (int i = 0; i < 25; i++)
-                if (i < bars)
-                    bar += "/";
-                else
-                    bar += "·";
-            return $"[{bar}]";
-        }
-
-        private void UpdateMonitorText()
-        {
-            Monitor.Clear();
-            Monitor.Text = _monitorText;
-
-            int start = 0;
-            for (int i = 0; i < _monitorBlocks; i++)
-            {
-                ApplyMonitorStyle(start, 10, Color.Orange);
-                ApplyMonitorStyle(start + 10, 1, Color.Pink);
-
-                string text = Monitor.Text.Substring(start + 11, 25);
-                int bars = text.LastIndexOf('/') + 1;
-                int dots = 25 - bars;
-
-                ApplyMonitorStyle(start + 11, bars, Color.Red);
-                ApplyMonitorStyle(start + 11 + bars, dots, Color.DarkGreen);
-                ApplyMonitorStyle(start + 36, 1, Color.Pink);
-                ApplyMonitorStyle(start + 37, 3, Color.Cyan);
-                ApplyMonitorStyle(start + 40, 1, Color.YellowGreen);
-
-                if (i % 2 == 0)
-                    start += 42;
-                else
-                    start += 43;
-            }
-        }
-
-        private void ApplyMonitorStyle(int start, int length, Color color)
-        {
-            Monitor.Select(start, length);
-            Monitor.SelectionColor = color;
-        }
-
-        private void Shell_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Shell_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Up)
-            {
-                //MessageBox.Show("Flecha arriba pulsada");
-            }
-            else if (e.KeyCode == Keys.Enter)
-            {
-                //MessageBox.Show("Enter pulsado");
-                //e.Handled = true; // Opcional: evita que se agregue una nueva línea
-            }
-            else if (e.KeyCode == Keys.Down)
-            {
-                //MessageBox.Show("Flecha abajo pulsada");
-            }
-        }
-
-        private void ShowMonitor()
-        {
-            string A = "  CPU     [AAAA BBBB CCCC DDDD EEEE ] 26% |";
-            string B = "| RAM     [AAAA BBBB CCCC DDDD EEEE ] 75%  ";
-            string C = "  DISK C  [AAAA BBBB CCCC DDDD EEEE ] 06% |";
-            string D = "| DISK E  [AAAA BBBB CCCC DDDD EEEE ] 32%  ";
-            string E = "  NET     [AAAA BBBB CCCC DDDD EEEE ] 87% |";
-            string F = "|                                          ";
-            Monitor.Text = A + B + Environment.NewLine + C + D + Environment.NewLine + E + F;
-
-            Monitor.Select(0, 10);
-            Monitor.SelectionColor = Color.Orange;
-
-            Monitor.Select(10, 1);
-            Monitor.SelectionColor = Color.Pink;
-
-            Monitor.Select(11, 25);
-            Monitor.SelectionColor = Color.Green;
-
-            Monitor.Select(36, 2);
-            Monitor.SelectionColor = Color.Pink;
-                
-            Monitor.Select(38, 2);
-            Monitor.SelectionColor = Color.Cyan;
-
-            Monitor.Select(40, 3);
-            Monitor.SelectionColor = Color.YellowGreen;
-
         }
 
         private void ShowShellHeader()
         {
-            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            cpuCounter.NextValue();
-            // Disco
-            //var diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
-            //diskCounter.NextValue();
-            //// RAM
-            //var ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-            //ramCounter.NextValue();
-
-            DriveInfo drive = new DriveInfo("C");
-            long totalDisk = drive.TotalSize;
-            long freeDisk = drive.AvailableFreeSpace;
-            long udiskUsage = totalDisk - freeDisk;
-            float diskUsage = (float)(((udiskUsage / 1024) / 1024) / 1024);
-            //Console.WriteLine($"Disco usado: {usedDisk / (1024 * 1024 * 1024)} GB de {totalDisk / (1024 * 1024 * 1024)} GB");
-
-            var ci = new ComputerInfo();
-            ulong totalRam = ci.TotalPhysicalMemory;
-            ulong availableRam = ci.AvailablePhysicalMemory;
-            ulong uramUsage = totalRam - availableRam;
-            float ramUsage = (float)((uramUsage / 1024) / 1024);
-            //Console.WriteLine($"RAM usada: {usedRam / (1024 * 1024)} MB de {totalRam / (1024 * 1024)} MB");
-
-            //var netCounter = new PerformanceCounter("Network Interface", "Bytes Total/sec", "Realtek USB GbE Family Controller");
-            Thread.Sleep(1000);
-
-            float cpuUsage = cpuCounter.NextValue();
-            //float ramUsage = ramCounter.NextValue();
-            //float diskUsage = diskCounter.NextValue();
-            //float netUsage = netCounter.NextValue();
-
-            ShowBar(cpuUsage);
-            ShowBar(ramUsage);
-            ShowBar(diskUsage);
-
             //Console.WriteLine($"CPU: {cpuUsage:F2} %");
             //Console.WriteLine($"RAM: {ramUsage:F2} %");
             //Console.WriteLine($"Disco: {diskUsage:F2} %");
@@ -350,55 +119,58 @@ namespace Shell._WinForms
             */
         }
 
-        private void ShowBar(float usage)
+        private void Shell_TextChanged(object sender, EventArgs e)
         {
-            string t = "";
-            int bars = (int)usage / 2 +1;
-            for (int i = 0; i < 50; i++)
+            _input = Shell.Text[_userLine.Length..];
+            if (_input.Length > 0 && _input.Contains(' '))
+                args = _input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void Shell_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
             {
-                if (i < bars)
-                    t += "/";
-                else
-                    t += "·";
+                //MessageBox.Show("Flecha arriba pulsada");
             }
-
-            Shell.Text = $"[{t}]";
-            Shell.Select(0, 1);
-            Shell.SelectionColor = Color.Orange;
-
-            Shell.Select(1, bars);
-            Shell.SelectionColor = Color.Green;
-
-            Shell.Select(bars + 1, 51 - bars);
-            Shell.SelectionColor = Color.Gray;
-
-            Shell.Select(51, 1);
-            Shell.SelectionColor = Color.Orange;
+            else if (e.KeyCode == Keys.Enter)
+            {
+                //MessageBox.Show("Enter pulsado");
+                //e.Handled = true; // Opcional: evita que se agregue una nueva línea
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                //MessageBox.Show("Flecha abajo pulsada");
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                //MessageBox.Show("Espacio pulsado");
+            }
         }
 
         private void ShowUserLine()
         {
-            Shell.Text = $"{_user}@{_domn} {_path} >>> ";
+            _userLine = $"{_user}@{_domn} {_path} >>> ";
+            Shell.Text = _userLine;
             // Pinta el usuario
-            Shell.Select(0, _userln - 1);
-            Shell.SelectionColor = Color.Orange;
+            ApplyShellStyle(0, _userln - 1, Color.GreenYellow);
             // Pinta la arroba
-            Shell.Select(_userln -1, 1);
-            Shell.SelectionColor = Color.DeepPink;
+            ApplyShellStyle(_userln - 1, 1, Color.DeepPink);
             // Pinta el dominio
-            Shell.Select(_userln, _domnln);
-            Shell.SelectionColor = Color.Orange;
+            ApplyShellStyle(_userln, _domnln, Color.LimeGreen);
             // Pinta la ruta
-            Shell.Select(_userln + _domnln, _pathln);
-            Shell.SelectionColor = Color.Coral;
+            ApplyShellStyle(_userln + _domnln, _pathln, Color.Coral);
             // Pinta las flechas del final
-            Shell.Select(_userln + _domnln + _pathln, 4);
-            Shell.SelectionColor = Color.BlueViolet;
+            ApplyShellStyle(_userln + _domnln + _pathln, 4, Color.BlueViolet);
             // Devuelve el resto del texto al color original
             Shell.SelectionStart = Shell.Text.Length;
             Shell.SelectionLength = 0;
-
             Shell.SelectionColor = Color.Green;
+        }
+
+        private void ApplyShellStyle(int start, int length, Color color)
+        {
+            Shell.Select(start, length);
+            Shell.SelectionColor = color;
         }
     }
 }
---
+++ Shell._WinForms/MonitorController.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Shell._WinForms
+{
+    internal class MonitorController
+    {
+        private readonly PerformanceCounter _receiveCounter;
+        private readonly PerformanceCounter _sendCounter;
+        private readonly PerformanceCounter _cpuCounter;
+        private readonly PerformanceCounter _ramCounter;
+        private readonly RichTextBox _monitor;
+        private DriveInfo[] _newDrives;
+        private DriveInfo[] _drives;
+        private static System.Timers.Timer? timer;
+
+        private const string DOWNARROW = "\u2191";
+        private const string UPARROW = "\u2193";
+        private string _monitorText = string.Empty;
+        private string _cpumonitor = string.Empty;
+        private string _rammonitor = string.Empty;
+        private string _upmonitor = string.Empty;
+        private string _downmonitor = string.Empty;
+        private int _monitorBlocks = 0;
+        private int _cpu = 0;
+        private int _ram = 0;
+        private int _up = 0;
+        private int _down = 0;
+
+        public MonitorController(RichTextBox monitor)
+        {
+            _monitor = monitor;
+
+            _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            _cpuCounter.NextValue();
+            _ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            _ramCounter.NextValue();
+            _sendCounter = new PerformanceCounter("Network Interface", "Bytes Sent/sec", "Realtek USB GbE Family Controller");
+            _sendCounter.NextValue();
+            _receiveCounter = new PerformanceCounter("Network Interface", "Bytes Received/sec", "Realtek USB GbE Family Controller");
+            _receiveCounter.NextValue();
+            _drives = DriveInfo.GetDrives();
+
+            Thread.Sleep(1000);
+            UpdateMonitorValues(null, null);
+            timer = new System.Timers.Timer(2000);
+            timer.Elapsed += UpdateMonitorValues;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        private void UpdateMonitorValues(object? sender, ElapsedEventArgs? e)
+        {
+            _monitorText = string.Empty;
+            _monitorBlocks = 0;
+
+            ReadComputerValues();
+            ReadDiskValues();
+
+            if (_monitor.InvokeRequired)
+                _monitor.Invoke(new Action(() => UpdateMonitorText()));
+            else
+                UpdateMonitorText();
+        }
+
+        private void ReadComputerValues()
+        {
+            _cpu = Convert.ToInt32(_cpuCounter.NextValue());
+            _ram = Convert.ToInt32(_ramCounter.NextValue());
+            _up = Convert.ToInt32(_sendCounter.NextValue()) / 1024;
+            _down = Convert.ToInt32(_receiveCounter.NextValue()) / 1024;
+
+            _cpumonitor = $"  CPU     {GetUsageBar(_cpu)} {_cpu:00}% ";
+            _rammonitor = $"  RAM     {GetUsageBar(_ram)} {_ram:00}% ";
+            _upmonitor  = $"  NET {UPARROW}   {GetUsageBar(_up)} {_up:00}% ";
+            _downmonitor = $"  NET {DOWNARROW}   {GetUsageBar(_down)} {_down:00}% ";
+
+            _monitorText = $"{_cpumonitor}{_rammonitor}\n{_upmonitor}{_downmonitor}\n";
+            _monitorBlocks = 4;
+        }
+
+        private void ReadDiskValues()
+        {
+            bool error = false;
+
+            for (int i = 0; i < _drives.Length; i++)
+            {
+                try
+                {
+                    DriveInfo drive = _drives[i];
+                    string dName = drive.Name[..1];
+                    float totalDisk = Convert.ToSingle(drive.TotalSize / 1024 / 1024 / 1024);
+                    float freeDisk = Convert.ToSingle(drive.AvailableFreeSpace / 1024 / 1024 / 1024);
+                    int usedDisk = Convert.ToInt32((totalDisk - freeDisk) * 100 / totalDisk);
+
+                    if ((i % 2) == 0)
+                        _monitorText += $"  UNIT {dName}  {GetUsageBar(usedDisk)} {usedDisk:00}% ";
+                    else
+                        _monitorText += $"  UNIT {dName}  {GetUsageBar(usedDisk)} {usedDisk:00}% \n";
+                    _monitorBlocks++;
+                }
+                catch (Exception)
+                {
+                    _newDrives = _drives.Where((value, index) => index != i).ToArray();
+                    error = true;
+                    continue;
+                }
+                finally
+                {
+                    if (error)
+                        _drives = _newDrives;
+                }
+            }
+        }
+
+        private static string GetUsageBar(int usage)
+        {
+            string bar = string.Empty;
+            int bars = usage / 4 < 1 ? 1 : usage / 4;
+            for (int i = 0; i < 25; i++)
+                if (i < bars)
+                    bar += "/";
+                else
+                    bar += "·";
+            return $"[{bar}]";
+        }
+
+        private void UpdateMonitorText()
+        {
+            _monitor.Clear();
+            _monitor.Text = _monitorText;
+
+            int start = 0;
+            for (int i = 0; i < _monitorBlocks; i++)
+            {
+                ApplyMonitorStyle(start, 10, Color.Orange);
+                ApplyMonitorStyle(start + 10, 1, Color.Pink);
+
+                string text = _monitor.Text.Substring(start + 11, 25);
+                int bars = text.LastIndexOf('/') + 1;
+                int dots = 25 - bars;
+
+                ApplyMonitorStyle(start + 11, bars, Color.Red);
+                ApplyMonitorStyle(start + 11 + bars, dots, Color.DarkGreen);
+                ApplyMonitorStyle(start + 36, 1, Color.Pink);
+                ApplyMonitorStyle(start + 37, 3, Color.Cyan);
+                ApplyMonitorStyle(start + 40, 1, Color.YellowGreen);
+
+                if (i % 2 == 0)
+                    start += 42;
+                else
+                    start += 43;
+            }
+        }
+
+        private void ApplyMonitorStyle(int start, int length, Color color)
+        {
+            _monitor.Select(start, length);
+            _monitor.SelectionColor = color;
+        }
+    }
+}
